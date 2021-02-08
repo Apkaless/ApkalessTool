5693ccf8cd604eae3b6472f4cee0c5e640947aa0
Apkaless <54741624+Apkaless@users.noreply.github.com>
2021-02-09 01:34:31 +0300

    Add files via upload

--- Apkaless.py
+++ Apkaless.py
@@ -25,7 +25,6 @@ try:
      import datetime
      from pydub import AudioSegment
      from pydub.playback import play
-     from pygame import mixer
      from playsound import playsound
 
      # code started
